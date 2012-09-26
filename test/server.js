e2daab170bae1e60f1900a24728bfdded9ea28a7
David Chambers <david@upthere.com>
2012-09-26 15:10:04 -0700

    test server: create Express server the new way

--- test/server.js
+++ test/server.js
@@ -1,5 +1,6 @@
-var app = require('express').createServer();
+var express = require('express');
 
+var app = express();
 app.get(/^\/((doc)?test[.](coffee|js))?$/, function (req, res) {
   res.sendfile(__dirname + '/' + (req.route.params[0] || 'test.html'));
 });
