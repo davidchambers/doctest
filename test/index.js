4b13b0381d42a1d57a1afcd4ba34525b15be5ae7
David Chambers <dc@davidchambers.me>
2023-02-17 09:02:53 +0100

    extract logging from main module

--- test/index.js
+++ test/index.js
@@ -40,6 +40,14 @@ const eq = actual => expected => {
   strictEqual (Z.equals (actual, expected), true);
 };
 
+const b = '\u001B[1m';
+const x = '\u001B[22m';
+
+const formatDescription = options => path => description => (
+  x + 'doctest (' + show (options) + ') (' + b + show (path) + x + ') › ' +
+  b + description
+);
+
 const Test = description => input => output => comparison => ({
   description,
   expected: {
@@ -66,19 +74,13 @@ const testModule = (module, path, options) => {
   let promise = null;
   const run = () => (promise ?? (promise = doctest (options) (path)));
 
-  const b = '\u001B[1m';
-  const x = '\u001B[22m';
-  const prefix = (
-    x + 'doctest (' + show (options) + ') (' + b + show (path) + x + ') › ' + b
-  );
-
-  test (prefix + '.length', async () => {
+  test (formatDescription (options) (path) ('.length'), async () => {
     const actuals = await run ();
     eq (actuals.length) (expecteds.length);
   });
 
   expecteds.forEach (({description, expected}, idx) => {
-    test (prefix + description, async () => {
+    test (formatDescription (options) (path) (description), async () => {
       const actuals = await run ();
       eq (actuals[idx]) (expected);
     });
@@ -104,59 +106,47 @@ const testCommand = (command, expected) => {
 };
 
 testModule (resultsSharedJs, 'test/shared/index.js', {
-  silent: true,
 });
 
 testModule (resultsSharedCoffee, 'test/shared/index.coffee', {
   coffee: true,
-  silent: true,
 });
 
 testModule (resultsLineEndings, 'test/line-endings/CR.js', {
-  silent: true,
 });
 
 testModule (resultsLineEndings, 'test/line-endings/CR.coffee', {
   coffee: true,
-  silent: true,
 });
 
 testModule (resultsLineEndings, 'test/line-endings/CR+LF.js', {
-  silent: true,
 });
 
 testModule (resultsLineEndings, 'test/line-endings/CR+LF.coffee', {
   coffee: true,
-  silent: true,
 });
 
 testModule (resultsLineEndings, 'test/line-endings/LF.js', {
-  silent: true,
 });
 
 testModule (resultsLineEndings, 'test/line-endings/LF.coffee', {
   coffee: true,
-  silent: true,
 });
 
 testModule (resultsExceptions, 'test/exceptions/index.js', {
-  silent: true,
 });
 
 testModule (resultsStatements, 'test/statements/index.js', {
-  silent: true,
 });
 
 testModule (resultsFantasyLand, 'test/fantasy-land/index.js', {
   module: 'esm',
-  silent: true,
 });
 
 testModule (resultsTranscribe, 'test/transcribe/index.js', {
   prefix: '.',
   openingDelimiter: '```javascript',
   closingDelimiter: '```',
-  silent: true,
 });
 
 testModule (resultsTranscribe, 'test/transcribe/index.coffee', {
@@ -164,70 +154,56 @@ testModule (resultsTranscribe, 'test/transcribe/index.coffee', {
   prefix: '.',
   openingDelimiter: '```coffee',
   closingDelimiter: '```',
-  silent: true,
 });
 
 testModule (resultsCommonJsRequire, 'test/commonjs/require/index.js', {
   module: 'commonjs',
-  silent: true,
 });
 
 testModule (resultsCommonJsExports, 'test/commonjs/exports/index.js', {
   module: 'commonjs',
-  silent: true,
 });
 
 testModule (resultsCommonJsModuleExports, 'test/commonjs/module.exports/index.js', {
   module: 'commonjs',
-  silent: true,
 });
 
 testModule (resultsCommonJsStrict, 'test/commonjs/strict/index.js', {
   module: 'commonjs',
-  silent: true,
 });
 
 testModule (resultsCommonJsDirnameJs, 'test/commonjs/__dirname/index.js', {
   module: 'commonjs',
-  silent: true,
 });
 
 testModule (resultsCommonJsDirnameCoffee, 'test/commonjs/__dirname/index.coffee', {
   module: 'commonjs',
   coffee: true,
-  silent: true,
 });
 
 testModule (resultsCommonJsFilenameJs, 'test/commonjs/__filename/index.js', {
   module: 'commonjs',
-  silent: true,
 });
 
 testModule (resultsCommonJsFilenameCoffee, 'test/commonjs/__filename/index.coffee', {
   module: 'commonjs',
   coffee: true,
-  silent: true,
 });
 
 testModule (resultsCommonJsDoctestRequire, 'test/commonjs/__doctest.require/index.js', {
   module: 'commonjs',
-  silent: true,
 });
 
 testModule (resultsBin, 'test/bin/executable', {
-  silent: true,
 });
 
 testModule (resultsEs2015, 'test/es2015/index.js', {
-  silent: true,
 });
 
 testModule (resultsEs2018, 'test/es2018/index.js', {
-  silent: true,
 });
 
 testModule (resultsEs2020, 'test/es2020/index.js', {
-  silent: true,
 });
 
 testModule (resultsEsmGlobals, 'test/esm/globals/index.js', {
@@ -236,13 +212,50 @@ testModule (resultsEsmGlobals, 'test/esm/globals/index.js', {
 });
 
 testModule (resultsContiguity, 'test/contiguity/index.js', {
-  silent: true,
 });
 
 testModule (resultsContiguity, 'test/contiguity/index.coffee', {
   coffee: true,
-  silent: true,
 });
+
+{
+  const options = {module: 'esm', print: true};
+  const path = 'test/esm/index.js';
+  test (formatDescription (options) (path) ('printing'), async () => {
+    const actual = await doctest (options) (path);
+    const expected = `
+
+
+__doctest.enqueue({
+  input: {
+    lines: [
+      {number: 3, text: "> toFahrenheit (0)"},
+    ],
+    thunk: () => {
+      return (
+        toFahrenheit (0)
+      );
+    },
+  },
+  output: {
+    lines: [
+      {number: 4, text: "32"},
+    ],
+    thunk: () => {
+      return (
+        32
+      );
+    },
+  },
+});
+
+export function toFahrenheit(degreesCelsius) {
+  return degreesCelsius * 9 / 5 + 32;
+}
+`;
+    eq (actual) (expected);
+  });
+}
 
 testCommand ('bin/doctest', {
   status: 0,
